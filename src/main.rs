8c9d0d44e648226f9fc608c8700fdad8fc32beed
alvaro0106 <alvarollvv@gmail.com>
2024-01-18 20:26:35 -0600

    Creacion de componentes

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,51 @@ fn main() {
 
 #[function_component(App)]
 fn app() -> Html {
-    let handle_input = Callback::from(|_| {});
 
     html!{
         <main>
-            <div>
-            {"Ingresa una palabra"}
-            </div>
-            <div>
-            <input type="text" oninput={handle_input}/> 
-            </div>
-            <div>
-            <button>{"busca!"}</button>
-            </div>
-            <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/pvc84wrTJBI?si=42oHI4YIN9k1fnj9"></iframe>
-            </div>
+            <VideoControls/>
+            <VideoSection name= "nombre video" id="pvc84wrTJBI?si=2SRySSjMjrJg3LfQ"/>
         </main>
     }
+}
 
+#[derive(Properties, PartialEq)]
+struct VideoControlsProps {
+    on_search: Callback<String>
+}
+
+#[function_component(VideoControls)]
+fn controls() -> Html{
+    let handle_input = Callback::from(|_| {});
+    html!{
+        <div>
+            <div>
+                {"Ingresa una palabra"}
+            </div>
+            <div>
+                <input type="text" oninput={handle_input}/> 
+            </div>
+            <div>
+                <button>{"busca!"}</button>
+            </div>
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct VideoSectionProps{
+    //Dos propiedades
+    id: String,
+    name: String,
+}
+
+#[function_component(VideoSection)]
+fn video_section(props: &VideoSectionProps) -> Html {
+    let yt_url: String =format!("https://www.youtube.com/embed/{}", props.id);
+    html!{
+        <div>
+            <iframe width="560" height="315" src={yt_url}></iframe>
+        </div>
+    }
 }
